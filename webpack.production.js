6521241d0cb479d92a449a9f94fdb049382f208a
Victor <azeta351@gmail.com>
2019-11-07 23:56:20 +0100

    chore: add autoprefixer for vendor prefixes

--- webpack.production.js
+++ webpack.production.js
@@ -13,6 +13,7 @@ module.exports = merge(baseConfig, {
             loader: MiniCssExtractPlugin.loader,
           },
           "css-loader",
+          "postcss-loader",
           "sass-loader",
         ],
       },
